2b4b232d6db687a8d5570138745e85c35d877642
Victor <vich.martins@gmail.com>
2017-04-12 11:05:46 -0400

    finished Time2 Overloading parameters adn contructors

--- Time2/Time2.java
+++ Time2/Time2.java
@@ -3,37 +3,37 @@
 
 public class Time2
 {
-	
+
 private int hour, minute, second;
 
 
 public Time2()
 {
-this(0, 0, 0);	
+this(0, 0, 0);
 
 }
 
 public Time2(int h)
 {
-this(h, 0, 0);	
+this(h, 0, 0);
 
 }
 
 public Time2(int h, int m)
 {
-this(h, m, 0);	
+this(h, m, 0);
 
 }
 
 public Time2(int h, int m, int s)
 {
-this(h, m, s);	
+setTime(h, m, s);
 
 }
 
 public Time2(Time2 time)
 {
-this(time.getHour(), time.getMinute(), time.getSecond());	
+this(time.getHour(), time.getMinute(), time.getSecond());
 
 }
 
@@ -42,12 +42,12 @@ public void setTime(int h, int m, int s)
 	setHour(h);
 	setMinute(m);
 	setSecond(s);
-	
+
 }
 
 public void setHour(int h)
 {
-if (h >= && h < 24)
+if (h >= 0 && h < 24)
 	hour = h;
 else
 	throw new IllegalArgumentException("hour must be 0-23");
@@ -56,7 +56,7 @@ else
 
 public void setMinute(int m)
 {
-if (m >= && m < 60)
+if (m >= 0 && m < 60)
 	minute = m;
 else
 	throw new IllegalArgumentException("minute must be  0-59");
@@ -66,13 +66,42 @@ else
 public void setSecond(int s)
 {
 if (s >= 0 && s < 60)
-	second = ((s >= 0 && s < 60)? s : 0)
+	second = ((s >= 0 && s < 60)? s : 0);
 else
-	throw new IllegalArgumentException("hour must be 0-23");
+	throw new IllegalArgumentException("second must be 0-59");
 
 }
 
-// end of page 321 on book.
+public int getHour()
+{
+	return hour;
 
+}
+
+public int getMinute()
+{
+	return minute;
+
+}
+
+public int getSecond()
+{
+	return second;
+}
+
+public String toString()
+{
+
+return String.format("%d:%02d:%02d %s",
+		((getHour() == 0 || getHour() == 12) ? 12 : getHour() % 12),
+		getMinute(), getSecond(), (getHour() < 12 ? "AM" : "PM"));
+
+}
+
+public String toUniversalString()
+{
+
+	return String.format("%d:%02d:%02d", getHour(), getMinute(), getSecond());
+}
 
 }
