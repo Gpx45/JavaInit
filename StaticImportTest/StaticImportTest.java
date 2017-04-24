8dfdc372a75d9b63a6a3cc02a5d90cf93cbc60d1
Victor <vich.martins@gmail.com>
2017-04-23 20:23:36 -0400

    added stuff

---
+++ StaticImportTest/StaticImportTest.java
@@ -0,0 +1,14 @@
+
+
+import static java.lang.Math.*;
+
+public class StaticImportTest
+{
+  public static void main(String[] args)
+  {
+    System.out.printf("sqrt(900.0) = %1.f\n", sqrt(900.00));
+    
+  }
+
+
+}
