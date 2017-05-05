bca5a63cd70b76e9c964cb78dde3a29c7da868ec
Victor <vich.martins@gmail.com>
2017-05-05 12:47:50 -0400

    added the rest

--- StaticImportTest/StaticImportTest.java
+++ StaticImportTest/StaticImportTest.java
@@ -7,7 +7,11 @@ public class StaticImportTest
   public static void main(String[] args)
   {
     System.out.printf("sqrt(900.0) = %1.f\n", sqrt(900.00));
-    
+    System.out.printf("ceil(-9.8) = %1.f\n", ceil(-9.8));
+    System.out.printf("sqrt(900.0) = %1.f\n", sqrt(900.00));
+
+    System.out.printf("sqrt(900.0) = %1.f\n", sqrt(900.00));
+
   }
 
 
